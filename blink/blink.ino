93021a702bd3eb70c2e0c94b46e72f61866237fa
Omer Jonuzi <omerjonuzi7@gmail.com>
2021-12-01 00:19:47 +0100

    Text to morse program done

--- blink/blink.ino
+++ blink/blink.ino
@@ -5,49 +5,149 @@ int unit = 100;
 int letterdelaytime = unit*3;
 int worddelaytime = unit*7;
 int beep = 9; // connect buzzer
-char arr[2] = "h";
+char arr[12] = "hello world"; //this is where you write what the arduino will say in morse. Remember correct array size
+
+//TODO: Base program now running. All that remains is to communicate with twitter
+// Start here: https://forum.arduino.cc/t/arduino-twitter-without-ethernet-shield/61386
+
+bool runOnce = true;
 
 void setup() {
      pinMode(beep, OUTPUT);
-     
-     //string st = "hello";
-     //the c_str() function returns a const pointer
-     //to null terminated contents.
-     //const char *str = st.c_str();
 }
 
 void loop() {
-//this is where you write what the arduino will say in morse.
-//write worddelaytime if you are doing another word.
-//a();
+ if (runOnce) {
+  runOnce = false;
+ 
 
-//h();
-
-for (int i = 0; i < sizeof(arr); i = i + 1) {
-  //conver character to int for use in switch statement
-  int x = arr[i] - '0';
-  
-    switch (x) {
-      case 'h':
-        h();
-        break;
-      case 'e':
-        e();
-        break;
-      case 'l':
-        l();
-        break;
-      case 'o':
-        o();
-        break;
-      default:
-        a();
-        break;
-    };
-}
+  for (int i = 0; i < sizeof(arr); i = i + 1) {
+    //conver character to int for use in switch statement
+    int x = arr[i];
+      //TODO: Figure out why it defaults
+      //hint: check what x is, and how case is interpreted.
+     switch (x) {
+          case char(32):
+              delay(worddelaytime);
+              break;
+          case 'a':
+              a();      
+              break;
+          case 'b':
+              b();
+              break;
+          case 'c':
+              c();
+              break;
+          case 'd':
+              d();
+              break;
+          case 'e':
+              e();
+              break;
+          case 'f':
+              f();
+              break;
+          case 'g':
+              g();
+              break;     
+          case 'h':
+              h();
+              break;
+          case 'i':
+              _i_(); //avoiding i() since it's a keyword
+              break;
+          case 'j':
+              j();
+              break;
+          case 'k': case '\0': // Take into account end of string '\0', here letter 'k' represents end of transmission
+              //k();
+              break;
+          case 'l':
+              l();
+              break;
+          case 'm':
+              m();
+              break;
+          case 'n':
+              n();
+              break;
+          case 'o':
+              o();
+              break;
+          case 'p':
+              p();
+              break;  
+          case 'q':
+              q();
+              break;
+          case 'r':
+              r();
+              break;
+          case 's':
+              s();
+              break;
+          case 't':
+              t();
+              break;
+          case 'u':
+              u();
+              break;  
+          case 'v':
+              v();
+              break;
+          case 'w':
+              w();
+              break;
+          case 'x':
+              _x_(); //avoiding x() since it's a keyword
+              break;
+          case 'y':
+              y();
+              break;
+          case 'z':
+              z();
+              break;
+       
+          case '0':
+              ZERO();
+              break;
+          case '1':
+              ONE();
+              break;
+          case '2':
+              TWO();
+              break;
+          case '3':
+              THREE();
+              break;
+          case '4':
+              FOUR();
+              break;
+          case '5':
+              FIVE();
+              break;
+          case '6':
+              SIX();
+              break;  
+          case '7':
+              SEVEN();
+              break;
+          case '8':
+              EIGHT();
+              break;
+          case '9':
+              NINE();
+              break;
+         default:
+          ZERO();
+          break;
+      };
+  }
 
 delay(letterdelaytime);
 delay(100000);
+ }
 }//loop
 
 void getLett(int x) {
@@ -143,7 +243,7 @@ void h() {
      pause();
 }//morseH
 
-void i() {
+void _i_() {
      dot();
      pause();
      dot();
@@ -277,7 +377,7 @@ void w() {
      pause();
 }//morseW
 
-void x() {
+void _x_() {
      dash();
      pause();
      dot();
